724c1c24205e20eff9d9bea120c3987b7b32efac
Kyle Maxwell <krmaxwell@gmail.com>
2013-08-17 16:18:11 -0500

    Switched to Solarized theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,20 +10,22 @@ TIMEZONE = 'US/Central'
 
 DEFAULT_LANG = u'en'
 
+THEME = 'pelican-solarized'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+#LINKS =  (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
 
